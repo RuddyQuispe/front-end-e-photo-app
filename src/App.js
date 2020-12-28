d872d16c3e2b1832cfd492f66e3bae3f2d9953df
RuddyQuispe <ruddyq18@gmail.com>
2020-12-28 02:56:14 -0400

    build: web template integrated sb-admin in project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
+import Content from "./components/template/Content";
+import Footer from "./components/template/Footer";
+import Navbar from "./components/template/Navbar";
+import ScrollbarModal from "./components/template/ScrollbarModal";
+import Sidebar from "./components/template/Sidebar";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="wrapper">
+        <Sidebar />
+        <div id="content-wrapper" className="d-flex flex-column">
+          <div id="content">
+            <Navbar />
+            <Content />
+          </div>
+          <Footer />
+        </div>
+      </div>
+      <ScrollbarModal />
     </div>
   );
 }
